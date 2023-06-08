faf703fe360b7ab73868496c98dc1ac8189daa76
truongphatroy <truongphatroy@gmail.com>
2023-06-09 05:06:49 +0900

    Delete ncc directory

--- ncc/src/UI/Wrapper.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import classes from "./Wrapper.module.scss";
-
-export const Wrapper = ({ children }) => {
-  return <div className={classes.WrapperStyle}>{children}</div>;
-};
